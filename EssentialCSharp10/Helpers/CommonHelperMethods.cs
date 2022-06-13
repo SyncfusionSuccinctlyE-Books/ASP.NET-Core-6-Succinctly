6fe27380d9f193a9d32a0f782577f6209319822f
Dirk Strauss <githubmine@outlook.com>
2022-06-13 08:49:05 +0200

    Added code sample for interpolated constants

--- EssentialCSharp10/Helpers/CommonHelperMethods.cs
+++ EssentialCSharp10/Helpers/CommonHelperMethods.cs
@@ -1,9 +1,21 @@
 ﻿namespace EssentialCSharp10.Helpers;
 public static class CommonHelperMethods
 {
+    const string SupportEmail = "support@email.com";
+    const string SupportNumber = "011-1234567";
+    const string SupportMessage = $"Please contact support at {SupportNumber} or email {SupportEmail}";
+    //const string SupportMessage = "Please contact support at "
+    //    + SupportNumber
+    //    + " or email "
+    //    + SupportEmail;
+
+
+
+
+
     public static string SanitizeString(string sInput)
     {
         // Do some string sanitization here
         return sInput;
-    }    
+    }
 }
